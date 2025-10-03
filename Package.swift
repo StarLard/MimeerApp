1c09a9a3891076c31fb98f667f6e6e8c8ba3ba61
Caleb Friden <13044247+StarLard@users.noreply.github.com>
2025-10-02 22:32:14 -0700

    Lock StarLardKit version

--- Package.swift
+++ Package.swift
@@ -23,9 +23,17 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/StarLard/StarLardKit.git", branch: "master"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", .upToNextMajor(from: "8.56.2")),
-        .package(url: "https://github.com/StarLard/SwiftFormatPlugins.git", .upToNextMajor(from: "1.1.0"))
+        .package(
+            url: "https://github.com/StarLard/StarLardKit.git",
+            .upToNextMajor(from: "1.0.0"),
+        ),
+        .package(
+            url: "https://github.com/getsentry/sentry-cocoa.git",
+            .upToNextMajor(from: "8.56.2"),
+        ),
+        .package(
+            url: "https://github.com/StarLard/SwiftFormatPlugins.git", .upToNextMajor(from: "1.1.0"),
+        )
     ],
     targets: [
         .target(
